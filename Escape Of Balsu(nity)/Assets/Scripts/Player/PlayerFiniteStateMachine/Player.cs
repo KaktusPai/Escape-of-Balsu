690ca8d1bbd796489ee34e288e203d2d84b138c3
KaktusPai <59810116+KaktusPai@users.noreply.github.com>
2022-02-07 17:50:06 +0100

    Movement and jumping SFM good progress

--- Escape Of Balsu(nity)/Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ Escape Of Balsu(nity)/Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -4,36 +4,90 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region State Variables
     public PlayerStateMachine StateMachine { get; private set; }
 
     public PlayerIdleState IdleState { get; private set; }
     public PlayerMoveState MoveState { get; private set; }
+    public PlayerJumpState JumpState { get; private set; }
+    public PlayerInAirState InAirState { get; private set; }
+    public PlayerLandState LandState { get; private set; }
 
-    public Animator Anim { get; private set; }
-    public PlayerInputHandler InputHandler { get; private set; }
-    
     [SerializeField]
     private PlayerData playerData;
+    #endregion
+
+    #region Components
+    public Animator Anim { get; private set; }
+    public PlayerInputHandler InputHandler { get; private set; }
+    public Rigidbody2D RB { get; private set; }
+    #endregion
+
+    #region Check Transforms
+    [SerializeField]
+    private Transform groundCheck;
+    #endregion
+
+    #region Other Variables
+    public Vector2 CurrentVelocity { get; private set; }
+
+    private Vector2 workspace;
+    #endregion
+
+    #region Unity Callback Functions
     private void Awake() 
     {
         StateMachine = new PlayerStateMachine();
 
         IdleState = new PlayerIdleState(this, StateMachine, playerData, "idle");
         MoveState = new PlayerMoveState(this, StateMachine, playerData, "move");
+        JumpState = new PlayerJumpState(this, StateMachine, playerData, "inAir");
+        InAirState = new PlayerInAirState(this, StateMachine, playerData, "inAir");
+        LandState = new PlayerLandState(this, StateMachine, playerData, "land");
     }
     private void Start() 
     {
         Anim = GetComponent<Animator>();
         InputHandler = GetComponent<PlayerInputHandler>();
+        RB = GetComponent<Rigidbody2D>();
 
         StateMachine.Initialize(IdleState); // Initialize statemachine with idle state
     }
     private void Update() 
     {
+        CurrentVelocity = RB.velocity;
         StateMachine.CurrentState.LogicUpdate();
     }
     private void FixedUpdate() 
     {
         StateMachine.CurrentState.PhysicsUpdate();
     }
+    #endregion
+
+    #region Set Functions
+    public void SetVelocityX(float velocity)
+    {
+        workspace.Set(velocity, CurrentVelocity.y);
+        RB.velocity = workspace;
+        CurrentVelocity = workspace;
+    }
+
+    public void SetVelocityY(float velocity)
+    {
+        workspace.Set(CurrentVelocity.x, velocity);
+        RB.velocity = workspace;
+        CurrentVelocity = workspace;
+    }
+    #endregion
+
+    #region Check Functions
+    public bool CheckIfGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, playerData.groundCheckRadius, playerData.whatIsGround);
+    }
+    #endregion
+
+    #region Other Functions
+
+    #endregion
 }
